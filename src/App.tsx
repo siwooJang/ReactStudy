6a65767c7e270a48497dc578732f396a013d3d60
siwooJang <joy8417@naver.com>
2023-06-22 02:14:09 +0900

    Redux 시계

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import {Provider as ReduxProvider} from 'react-redux'
+import {useStore} from './store'
+
+import ReduxClock from './pages/ReduxClock'
+import UseReducerClock from './pages/UseReducerClock'
+
+export default function App() {
+  const store = useStore()
+  return (
+    <ReduxProvider store={store}>
+      <main className="p-8">
+        <UseReducerClock />
+        <ReduxClock />
+        <div> siwoo Jang </div>
+      </main>
+    </ReduxProvider>
+  )
+}
